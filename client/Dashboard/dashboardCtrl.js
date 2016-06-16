e2e2e29deba996b4c0e44a8a3a77c92c831d56f4
Antonisfk <antonisfkiaras@gmail.com>
2016-06-15 23:14:35 -0700

    fixed bug

--- client/Dashboard/dashboardCtrl.js
+++ client/Dashboard/dashboardCtrl.js
@@ -9,42 +9,11 @@ angular.module('myApp')
     $scope.tempatures = [];
     DataService.index(function(result){
       $scope.tempatures = result;
-      $scope.data = result
+
       console.log($scope.tempatures)
     })
 
-      console.log($scope.tempatures)
-
-
-scope.options = {
-            chart: {
-                type: 'lineWithFocusChart',
-                height: 450,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 60,
-                    left: 40
-                },
-                duration: 500,
-                useInteractiveGuideline: true,
-                xAxis: {
-                    axisLabel: 'X Axis',
-                    tickFormat: function(d, i ){
-                      console.log(d)
-                      return d
-                    }
-                },
-                yAxis: {
-                    axisLabel: 'Y Axis',
-                    tickFormat: function(d, i){
-                      return d
-                    },
-                    rotateYLabel: false
-                }
-                }
-
-            }
+  
     $scope.liveTemp = function(){
       DataService.getTemp(function(result){
         console.log('alltemps',result) 
